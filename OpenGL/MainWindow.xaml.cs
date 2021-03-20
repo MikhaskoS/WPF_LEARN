6ee8f30e2e936c110b152ad9e6fd6d7c6b927ca0
MikhaskoS <mikhassergej@yandex.ru>
2021-03-20 13:13:28 +0300

    OpenTK

---
+++ OpenGL/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Wpf;
+using System;
+using System.Windows;
+
+
+namespace OpenGL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var settings = new GLWpfControlSettings
+            {
+                MajorVersion = 2,
+                MinorVersion = 1
+            };
+            OpenTkControl.Start(settings);
+        }
+
+        private void OpenTkControl_OnRender(TimeSpan delta)
+        {
+            GL.ClearColor(Color4.Blue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.LoadIdentity();
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color4(Color4.Red);
+            GL.Vertex2(0.0f, 0.5f);
+
+            GL.Color4(Color4.Green);
+            GL.Vertex2(0.58f, -0.5f);
+
+            GL.Color4(Color4.Blue);
+            GL.Vertex2(-0.58f, -0.5f);
+
+            GL.End();
+            GL.Finish();
+        }
+    }
+}
