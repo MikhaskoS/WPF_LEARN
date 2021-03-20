a2b2242cd05a3d4de067f8f4addbef88bf8d68d6
MikhaskoS <mikhassergej@yandex.ru>
2021-03-20 20:23:44 +0300

    ListView

---
+++ ListViewBinding/Data/TestData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ListViewBinding.Models;
+
+namespace ListViewBinding.Data
+{
+    static class TestData
+    {
+        public static List<Employee> Employees { get; } = Enumerable.Range(1, 100)
+            .Select(i => new Employee
+            {
+                Id = i,
+                Name = $"Имя {i}",
+                SurName = $"Фамилия {i}",
+                Patronymic = $"Отчество {i}",
+                DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365/6*(i+18)))
+            })
+            .ToList();
+    }
+}
---
+++ ListViewBinding/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ListViewBinding.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace ListViewBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Добавление вручную
+            //EmployeeList.Items.Add(new ListViewItem());
+            
+            //Данные отобразятся так, как будет переопределен ToString()
+            //EmployeeList.ItemsSource = TestData.Employees;
+        }
+    }
+}
