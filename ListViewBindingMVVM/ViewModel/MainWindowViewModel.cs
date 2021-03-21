6a012dc1a580692410eecddd0737cbbff64f10de
MikhaskoS <mikhassergej@yandex.ru>
2021-03-22 00:37:47 +0300

    MVVM

---
+++ ListViewBindingMVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ListViewBindingMVVM.ViewModel
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        private string _title = "Заголовок окна";
+        private ObservableCollection<EmployeeViewModel> _employees =
+            new ObservableCollection<EmployeeViewModel>();
+        private EmployeeViewModel _selectedEmploye;
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        public ObservableCollection<EmployeeViewModel> Employees
+        {
+            get => _employees;
+            set => Set(ref _employees, value);
+        }
+        public EmployeeViewModel SelectedEmploye
+        {
+            get => _selectedEmploye;
+            set => Set(ref _selectedEmploye, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            foreach (var employee in Enumerable.Range(1, 100).Select(i => new EmployeeViewModel
+            {
+                Id = i,
+                Name = $"Имя {i}",
+                SurName = $"Фамилия {i}",
+                Patronymic = $"Отчество {i}",
+                DayOfBirth = DateTime.Now.Subtract(TimeSpan.FromDays(365 / 6 * (i + 18)))
+            }))
+                _employees.Add(employee);
+        }
+    }
+}
